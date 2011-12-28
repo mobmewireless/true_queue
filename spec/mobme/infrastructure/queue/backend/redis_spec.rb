d6f70b71468061837c1a2933e26b0f6e51a68af2
Hari Gopal <hari@mobme.in>
2011-12-28 16:52:29 +0530

    Adding test to ensure network delays do not affect integrity of add operations.

--- spec/mobme/infrastructure/queue/backend/redis_spec.rb
+++ spec/mobme/infrastructure/queue/backend/redis_spec.rb
@@ -11,4 +11,42 @@ describe MobME::Infrastructure::Queue::Backends::Redis do
   
   it_behaves_like "a queue"
   it_behaves_like "a reserved queue"
+
+  context "in the presence of network delays" do
+    class MobME::Infrastructure::Queue::Backends::Redis
+      alias_method :write_value_original, :write_value
+    end
+
+    describe "#add" do
+      before :each do
+        queue.empty 'queue'
+        queue.stub(:generate_uuid).and_return(1)
+        queue.stub(:write_value) { |*args|
+          sleep 0.4
+          queue.send(:write_value_original, *args)
+        }
+      end
+
+      it "does not yield item until value is properly stored" do
+        adding_thread = Thread.new { queue.add 'queue', 'thrift' }
+
+        removing_thread = Thread.new do
+          extracted_items = []
+
+          while extracted_items.empty?
+            queue.remove('queue') do |item|
+              extracted_items << item
+            end
+
+            sleep 0.1
+          end
+
+          extracted_items.should_not include(nil)
+        end
+
+        adding_thread.join
+        removing_thread.join
+      end
+    end
+  end
 end
